d08cf1426f1d1b65b9a8f618fefd6dcbfd5b2e91
mercier <mercier@adacore.com>
2018-01-24 17:34:04 +0100

    Add binary expressions parsing

--- parser.ml
+++ parser.ml
@@ -2,14 +2,22 @@
  * is defined *)
 let binop_precedence: (char, int) Hashtbl.t = Hashtbl.create 10
 
-(* precedence - Get the precedence of the pending binary operator token. *)
-let precedence c = if Hashtbl.mem binop_precedence c then
-                     Some (Hashtbl.find binop_precedence c)
-                   else
-                     None
+(* by default, associativity is left *)
+let right_associative: (char, bool) Hashtbl.t = Hashtbl.create 10
+
+
+(* precedence - Get the precedence of the pending binary operator token.
+ *              c should be known (is_known c) *)
+let precedence c = Hashtbl.find binop_precedence c
+
+let is_left_associative c = not (Hashtbl.mem right_associative c
+                                 && Hashtbl.find right_associative c)
+
+(* is_known - Return true if the operator is known *)
+let is_known c = Hashtbl.mem binop_precedence c
 
 let rec parse_expr = parser
-  | [< p=parse_primary; stream >] -> p
+  | [< p=parse_primary; stream >] -> parse_bin_rhs 0 p stream
   | [< >] -> raise (Stream.Error "not expecting end of stream")
 
 (* primary 
@@ -29,25 +37,43 @@ and parse_primary = parser
   (* expecting a primary expression *)
   | [< >] -> raise (Stream.Error "unknown token when expecting an expression.")
 
-and parse_lhs = parser
-  | [< prim=parse_primary; stream >] ->
-      
+(*************************************************************************)
+(********************** Binary expression parsing ************************)
+(*************************************************************************)
 
-(* try to parse the rhs of a binop expression *)
-and parse_rhs lhs_precedence lhs_expr = parser
-  | [< 'Token.Kwd op; prim=parse_primary; stream >] ->
-      match precedence op with
-      | Some op_precedence ->
-          (* Greater or equal means left associativity *)
-          if lhs_precedence >= op_precedence then
-            parse_rhs lhs_expr
-          else
-            parse_rhs op_precedence prim
+and parse_bin_rhs prec lhs stream =
+  match Stream.peek stream with
+  | Some (Token.Kwd op) when is_known op ->
+      let op_prec = precedence op in
 
-      
-      (* It's a no match if the operator is not recognized *)
-      | None -> raise Stream.Failure;
+      if op_prec < prec then lhs else begin
+        (* eat the operator *)
+        Stream.junk stream;
 
+        (* get the next primary expression *)
+        let rhs = parse_primary stream in
+
+        let rhs =
+          (* lookahead for the next operator and its precedence *)
+          match Stream.peek stream with
+          | Some (Token.Kwd op2) when is_known op2 ->
+              let op2_prec = precedence op2 in
+              (* if the precedence of this operator is less than the previous,
+               * the rhs is just the primary expression, else we recurse to
+               * get the rhs *)
+              if op2_prec <= op_prec
+              then rhs
+              else parse_bin_rhs op_prec rhs stream
+          | _ -> rhs
+        in
+
+        let bin = Ast.Binary (op, lhs, rhs) in
+        parse_bin_rhs prec bin stream
+      end
+  | _ -> lhs
+(*************************************************************************)
+(******************** End binary expression parsing **********************)
+(*************************************************************************)
 
 
 (*==========================================================================*)
@@ -56,18 +82,26 @@ and parse_rhs lhs_precedence lhs_expr = parser
 
 let rec print_ast = parser
   | [< e=parse_expr; stream >] ->
-      begin
-        match e with
+      let rec aux = function
         | Ast.Number n ->
             print_int n;
-            print_char ' '
 
         (* variables *)
         | Ast.Variable id ->
-            print_string (id ^ " ")
+            print_string id
+
+        | Ast.Binary(op, lhs, rhs)  ->
+            print_char '(';
+            aux lhs;
+            print_char ' ';
+            print_char op;
+            print_char ' ';
+            aux rhs;
+            print_char ')'
 
         | _ -> assert false
-      end;
+      in
+      aux e;
       print_endline "";
       print_ast stream
 
@@ -80,7 +114,7 @@ let rec print_ast = parser
   | Call of string * expr list*)
 
 let main () =
-  Hashtbl.add binop_precedence '/' 10;
+  Hashtbl.add binop_precedence '<' 10;
   Hashtbl.add binop_precedence '+' 20;
   Hashtbl.add binop_precedence '-' 20;
   Hashtbl.add binop_precedence '*' 40;
